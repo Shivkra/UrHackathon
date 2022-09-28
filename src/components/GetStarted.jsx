0cf6dd68899a5d55d54deb3196bc7ce66620a144
sivasankarkk <109886104+sivasankarkk@users.noreply.github.com>
2022-09-28 15:34:56 +0530

    Add files via upload

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,24 @@
+import styles from "../style";
+import React from "react";
+
+const GetStarted = () => {
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+}, []);
+  return (
+      <div 
+      	className="apply-button" 
+      	data-hackathon-slug="urweb3hackathon" 
+      	data-button-theme="dark-inverted"
+      ></div>
+    )
+  };
+
+export default GetStarted;
